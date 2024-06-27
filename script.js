1de32ad619b4402e256f46d6f908e1403898fc26
Minhaz Halim Zim <31536403+minhazhalim@users.noreply.github.com>
2024-06-27 17:32:40 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const select = document.querySelectorAll("form select");
-const fromCurrency = document.querySelector(".from select");
-const toCurrency = document.querySelector(".to select");
-const button = document.querySelector("form button");
+const select = document.querySelectorAll('form select');
+const fromCurrency = document.querySelector('.from select');
+const toCurrency = document.querySelector('.to select');
+const button = document.querySelector('form button');
 const icon = document.querySelector('form .icon');
 for(let i = 0;i < select.length;i++){
     for(let currency_code in country_list){
@@ -23,7 +23,7 @@ function loadFlag(element){
 }
 function getExchangeRate(){
     const input = document.querySelector('form input');
-    const exchangeRate = document.querySelector("form .exchange-rate");
+    const exchangeRate = document.querySelector('form .exchange-rate');
     let amountValue = input.value;
     if(amountValue == "" || amountValue == '0'){
         input.value = '1';
@@ -32,9 +32,9 @@ function getExchangeRate(){
     exchangeRate.innerText = 'Getting Exchange Rate...';
     let url = `https://v6.exchangerate-api.com/v6/5d191a7bdaaf071c178ea20a/latest/${fromCurrency.value}`;
     fetch(url).then(response => response.json()).then(result =>{
-        let exchangeRate = result.conversion_rates[toCurrency.value];
-        let totalExRate = (amountValue * exchangeRate).toFixed(2);
-        exchangeRate.innerText = `${amountValue} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+        let exchangeRateResult = result.conversion_rates[toCurrency.value];
+        let totalExchangeRate = (amountValue * exchangeRateResult).toFixed(2);
+        exchangeRate.innerText = `${amountValue} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;
     }).catch(() => {
         exchangeRate.innerText = 'Something Went Wrong';
     });
